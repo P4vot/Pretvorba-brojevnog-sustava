388e35c6711a961a531929c8e9b073922a349b87
P4vot <pavo.lucic.205@gmail.com>
2025-09-02 11:17:17 +0200

    Update header.h

--- Project43/header.h
+++ Project43/header.h
@@ -1,67 +1,56 @@
+#pragma once
 #ifndef HEADER_H
 #define HEADER_H
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
 
-/* ===== Strukture ===== */
+// Konstante za vrste konverzija
+#define DEC_U_BIN "Decimalni u Binarni"
+#define BIN_U_DEC "Binarni u Decimalni"
+#define DEC_U_OKT "Decimalni u Oktalni"
+#define OKT_U_DEC "Oktalni u Decimalni"
+#define HEX_U_BIN "Heksadecimalni u Binarni"
+#define BIN_U_HEX "Binarni u Heksadecimalni"
+
+// Struktura za konverzije
 typedef struct {
-    char vrsta[50];
+    char vrsta[30];
     char ulaz[100];
     char izlaz[100];
 } Konverzija;
 
-/* Čvor za jednostruko povezani popis */
-typedef struct Cvor {
-    Konverzija podatak;
-    struct Cvor* sljedeci;
-} Cvor;
-
-/* ===== Globalne varijable (extern deklaracije) ===== */
+// Globalne varijable
 extern Konverzija* globalneKonverzije;
 extern int brojKonverzija;
 extern int maxKonverzija;
-extern Cvor* glavaListe;
 
-/* ===== Pomoćne inline/statik funkcije ===== */
-static inline void preokreniNiz(char* s) {
-    if (!s) return;
-    int i = 0, j = (int)strlen(s) - 1;
-    while (i < j) {
-        char c = s[i];
-        s[i] = s[j];
-        s[j] = c;
-        ++i; --j;
-    }
-}
+// Funkcije za provjeru validnosti brojeva
+int validanDecimalni(const char* ulaz);
+int validanBinarni(const char* ulaz);
+int validanOktalni(const char* ulaz);
+int validanHeksadecimalni(const char* ulaz);
 
-/* ===== Funkcije konverzija ===== */
-char* decimalniUBinarni(int decimalni);
-int   binarniUDekadski(const char* binarni);
-char* decimalniUoktalni(int decimalni);
-int   oktalniUDekadski(const char* oktalni);
-char* heksadekadskiUBinarni(const char* heks);
-char* binarniUHeksadekadski(const char* binarni);
+// Funkcije za konverzije
+char* decimalniUBinarni(int decimal);
+int binarniUDecimal(const char* binarni);
+char* decimalniUOktalni(int decimal);
+int oktalniUDecimal(const char* oktal);
+char* heksadecimalniUBinarni(const char* hex);
+char* binarniUHeksadecimalni(const char* binarni);
 
-/* ===== Rad s dinamičkim nizom ===== */
-void dodajKonverziju(const char* vrsta, const char* ulaz, const char* izlaz);
-void prikaziKonverzije(void);
+// Funkcije za upravljanje konverzijama
+void dodajKonverziju(const char* vrsta, const char* ulaz, const char* rezultat);
+void pregledajKonverzije();
 void azurirajKonverziju(int indeks, const char* noviUlaz);
-void obrisiKonverziju(int indeks);
-void spremiKonverzijeUDatoteku(const char* datoteka);
-void obrisiDatoteku(const char* datoteka);
-void sortirajKonverzijePoVrsti(void);
-void pretraziKonverzije(const char* vrsta);
-const Konverzija* pronadiKonverzijuBsearch(const char* vrsta);
-void rekurzivnaPretraga(int lijevo, int desno, const char* vrsta);
+void izbrisiKonverziju(int indeks);
+int usporediKonverzije(const void* a, const void* b);
+void rekurzivniQuickSort(Konverzija* polje, int lijevo, int desno, int (*usporedi)(const void*, const void*));
+int pronadiSveKonverzije(const char* vrsta, Konverzija** rezultati);
 
-/* ===== Rad s povezanim popisom ===== */
-void listaDodajKonverziju(const char* vrsta, const char* ulaz, const char* izlaz);
-void listaPrikaziKonverzije(void);
-void listaObrisiKonverziju(const char* vrsta, const char* ulaz);
-void listaOslobodi(void);
+// Funkcije za datoteke
+void spremiKonverzijeUDatoteku(const char* nazivDatoteke);
+void obrisiDatoteku(const char* nazivDatoteke);
 
-#endif /* HEADER_H */
+#endif // HEADER_H
